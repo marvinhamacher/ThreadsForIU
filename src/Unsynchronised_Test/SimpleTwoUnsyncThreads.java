e859c70501ed70b18a8e7eb01175bb77c6fa865e
Johann110 <johann.shilinski2002@gmail.com>
2024-04-05 16:46:25 +0200

    SimpleTwoUnsyncThreads

---
+++ src/Unsynchronised_Test/SimpleTwoUnsyncThreads.java
@@ -0,0 +1,42 @@
+package Unsynchronised_Test;
+
+public class SimpleTwoUnsyncThreads{
+    public static void main(String []args){
+        Thread t1 = new Thread(new A(), "countA");
+        System.out.println("Thread has been created: " + t1.getName());
+        Thread t2 = new Thread(new B(), "countB");
+        System.out.println("Thread has been created: " + t2.getName());
+
+        System.out.println("start counting");
+        t1.start();
+        t2.start();
+    }
+}
+
+class A implements Runnable {
+    @Override
+    public void run(){
+        for(int i = 0; i < 100; i++){
+            System.out.println("A: " + i);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class B implements Runnable {
+    @Override
+    public void run(){
+        for(int i = 0; i < 100; i++){
+            System.out.println("B: " + i);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
